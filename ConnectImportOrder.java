e96ab4bfd419f0b2028549cd6d7e6c2fb3b62877
dimmi213 <94824362+dimmi213@users.noreply.github.com>
2022-11-18 23:16:56 +0700

    Update ConnectImportOrder.java

--- ConnectImportOrder.java
+++ ConnectImportOrder.java
@@ -1,67 +1,316 @@
-package Connection;
+package GiaoDien;
 
+import java.awt.EventQueue;
+import java.util.ArrayList;
+import java.util.Vector;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableModel;
+
+import Connection.ConnectImportOrder;
+import Main.importOrder;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JOptionPane;
+
+import java.awt.Font;
+import javax.swing.SwingConstants;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JScrollPane;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import Main.importOrder;
 
-//import GiaoDien.ImportOrder;
+public class ImportOder extends JFrame {
+	private ArrayList<importOrder> list;
+	DefaultTableModel model;
+	private JPanel contentPane;
+	private JTable tableImportOrder;
+	private JTextField tfProductID;
+	private JTextField tfName;
+	private JTextField tfType;
+	private JTextField tfPrice;
+	private JTextField tfAmount;
+	
+	private JScrollPane scrollPane;
 
-public class ConnectImportOrder {
-	private Connection conn;
-	public ConnectImportOrder() {
-		try {
-			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-			conn = DriverManager.getConnection("jdbc:sqlserver://localhost:1433;databaseName=ImportOrder;user=sa;password=1234");
-		} catch (ClassNotFoundException | SQLException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
-		System.out.print(conn);
-	}
-	public boolean addProducts(importOrder i) {
-		String sql = "INSERT INTO tblImportOrder(ProductID, Name, Type, Price, Amount)"
-				+ "VALUES(?,?,?,?,?)";
-		try {
-			PreparedStatement ps = conn.prepareStatement(sql);
-			ps.setString(1, i.getId());
-			ps.setString(2, i.getName());
-			ps.setString(3, i.getType());
-			ps.setLong(4, i.getPrice());
-			ps.setLong(5, i.getAmount());
-			return  ps.executeUpdate() > 0;
-		} catch (Exception e) {
-			// TODO: handle exception
-			e.printStackTrace();
-		}
-		return false;
-	} 
-	public ArrayList<importOrder>getListimportOrder(){
-		ArrayList<importOrder> list = new ArrayList <>();
-		String sql = "SELECT * FROM tblImportOrder";
-		try {
-			PreparedStatement ps = conn.prepareStatement(sql);
-            ResultSet rs = ps.executeQuery();
-            while(rs.next()) {
-            	importOrder i = new importOrder();
-            	i.setId(rs.getString("ProductID"));
-            	i.setName(rs.getString("Name"));
-            	i.setType(rs.getString("Type"));
-            	i.setPrice(rs.getLong("Price"));
-            	i.setAmount(rs.getLong("Amount"));
-            	
-            	list.add(i);
-            }
-		} catch (Exception e) {
-			// TODO: handle exception
-			e.printStackTrace();
-		}
-		return list;
-	}
+	/**
+	 * Launch the application.
+	 */
 	public static void main(String[] args) {
-		new ConnectImportOrder();
+
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ImportOder frame = new ImportOder();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+	
+
+
+	/**
+	 * Create the frame.
+	 */
+	public void showTable() {
+		for(importOrder i : list) {
+			model.addRow(new Object[] {
+					j++,i.getIdI(),i.getId(), i.getName(), i.getType(), i.getPrice(), i.getAmount()
+			});
+		}
+	}
+	Connection conn = null;
+	ResultSet rs = null;
+	PreparedStatement ps = null;
+	int q;
+	public ImportOder() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 1379, 847);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel LabelImportOrder = new JLabel("IMPORT ORDER");
+		LabelImportOrder.setHorizontalAlignment(SwingConstants.CENTER);
+		LabelImportOrder.setFont(new Font("Bookman Old Style", Font.BOLD, 26));
+		LabelImportOrder.setBounds(10, 11, 251, 48);
+		contentPane.add(LabelImportOrder);
+		
+		scrollPane = new JScrollPane();
+		
+		scrollPane.setBounds(492, 59, 673, 548);
+		contentPane.add(scrollPane);
+		
+		tableImportOrder = new JTable();
+		tableImportOrder.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				DefaultTableModel model = (DefaultTableModel)tableImportOrder.getModel();
+				int row = tableImportOrder.getSelectedRow();
+				tfImportOrderID.setText(model.getValueAt(row, 1).toString());
+				tfProductID.setText(model.getValueAt(row, 2).toString());
+				tfName.setText(model.getValueAt(row, 3).toString());
+				tfType.setText(model.getValueAt(row, 4).toString());
+				tfPrice.setText(model.getValueAt(row, 5).toString());
+				tfAmount.setText(model.getValueAt(row, 6).toString());
+				
+			}
+		});
+		JLabel lblNewLabel = new JLabel("Product ID:");
+		lblNewLabel.setBounds(10, 123, 83, 14);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("Name:");
+		lblNewLabel_1.setBounds(10, 161, 83, 14);
+		contentPane.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("Type:");
+		lblNewLabel_2.setBounds(10, 201, 83, 14);
+		contentPane.add(lblNewLabel_2);
+		
+		JLabel lblNewLabel_3 = new JLabel("Price:");
+		lblNewLabel_3.setBounds(10, 239, 83, 14);
+		contentPane.add(lblNewLabel_3);
+		
+		JLabel lblNewLabel_4 = new JLabel("Amount:");
+		lblNewLabel_4.setBounds(10, 280, 83, 14);
+		contentPane.add(lblNewLabel_4);
+		
+		tfProductID = new JTextField();
+		tfProductID.setBounds(120, 120, 293, 20);
+		contentPane.add(tfProductID);
+		tfProductID.setColumns(10);
+		
+		tfName = new JTextField();
+		tfName.setBounds(120, 158, 293, 20);
+		contentPane.add(tfName);
+		tfName.setColumns(10);
+		
+		tfType = new JTextField();
+		tfType.setBounds(120, 198, 293, 20);
+		contentPane.add(tfType);
+		tfType.setColumns(10);
+		
+		tfPrice = new JTextField();
+		tfPrice.setBounds(120, 236, 293, 20);
+		contentPane.add(tfPrice);
+		tfPrice.setColumns(10);
+		
+		tfAmount = new JTextField();
+		tfAmount.setBounds(120, 277, 293, 20);
+		contentPane.add(tfAmount);
+		tfAmount.setColumns(10);
+		
+		JButton btnAdd = new JButton("Add");
+		btnAdd.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(tfImportOrderID.getText().equals("") || tfProductID.getText().equals("") || tfName.getText().equals("") || tfType.getText().equals("") || tfPrice.getText().equals("") || tfAmount.getText().equals("")) {
+					JOptionPane.showMessageDialog(rootPane, "Please fill complete information");
+				}
+				else {
+					importOrder i = new importOrder();
+					i.setIdI(tfImportOrderID.getText());
+					i.setId(tfProductID.getText());
+					i.setName(tfName.getText());
+					i.setType(tfType.getText());
+					i.setPrice(Long.parseLong(tfPrice.getText()) );
+					i.setAmount(Long.parseLong(tfAmount.getText()));
+					if(new ConnectImportOrder().addProducts(i)) {
+						list.add(i);
+						JOptionPane.showMessageDialog(rootPane, "Save Successfully");
+						showResult();
+						tfImportOrderID.setText("");
+						tfProductID.setText("");
+						tfName.setText("");
+						tfType.setText("");
+						tfPrice.setText("");
+						tfAmount.setText("");
+					} 
+					else {
+						JOptionPane.showMessageDialog(rootPane, "Product's ID cannot be duplicated!");
+					}
+					
+				}
+				
+				
+			}
+		});
+		btnAdd.setBounds(29, 328, 89, 23);
+		contentPane.add(btnAdd);
+		
+		JButton btnUpdate = new JButton("Update");
+		btnUpdate.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				int row = tableImportOrder.getSelectedRow();
+				if(row >=0) {
+					try {
+		
+						conn = DriverManager.getConnection("jdbc:sqlserver://localhost:1433;databaseName=ImportOrder;user=sa;password=1234");
+						String value =(tableImportOrder.getModel().getValueAt(row, 0).toString());
+						String query = "update tblImportOrder set ProductID=?,Name=?,Type=?,Price=?,Amount=? where ImportOrderID=?";
+						PreparedStatement ps = conn.prepareStatement(query);
+						ps = conn.prepareStatement(query);
+						ps.setString(6, tfImportOrderID.getText());
+						ps.setString(1, tfProductID.getText());
+						ps.setString(2, tfName.getText());
+						ps.setString(3, tfType.getText());
+						ps.setString(4, tfPrice.getText());
+						ps.setString(5, tfAmount.getText());
+						ps.executeUpdate();
+						model.setValueAt(tfImportOrderID.getText(), row, 1);
+						model.setValueAt(tfProductID.getText(), row, 2);
+						model.setValueAt(tfName.getText(), row, 3);
+						model.setValueAt(tfType.getText(), row, 4);
+						model.setValueAt(tfPrice.getText(), row, 5);
+						model.setValueAt(tfAmount.getText(), row, 6);
+						JOptionPane.showMessageDialog(null, "Update Successfully");
+						
+					} catch (Exception e2) {
+						// TODO: handle exception
+					}
+					
+				}
+				else {
+					JOptionPane.showMessageDialog(null, "Plese select a row first");
+				}
+			}
+		});
+		btnUpdate.setBounds(215, 328, 89, 23);
+		contentPane.add(btnUpdate);
+		
+		JButton btnDelete = new JButton("Delete");
+		btnDelete.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				int row = tableImportOrder.getSelectedRow();
+
+				if(row>=0) {
+					try {
+						conn = DriverManager.getConnection("jdbc:sqlserver://localhost:1433;databaseName=ImportOrder;user=sa;password=1234");
+						String value =(tableImportOrder.getModel().getValueAt(row, 0).toString());
+						String query = "delete tblImportOrder where ImportOrderID=?";
+						PreparedStatement ps = conn.prepareStatement(query);
+						ps = conn.prepareStatement(query);
+						ps.setString(1, tfImportOrderID.getText());
+						ps.executeUpdate();
+						model.removeRow(row);
+						JOptionPane.showMessageDialog(null, "Delete Successfully");
+					} catch (SQLException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+
+				}
+				else {
+					JOptionPane.showMessageDialog(null, "Plese select a row first");
+				}
+			}
+		});
+		btnDelete.setBounds(29, 376, 89, 23);
+		contentPane.add(btnDelete);
+		
+		JButton btnClear = new JButton("Clear");
+		btnClear.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				tfImportOrderID.setText("");
+				tfProductID.setText("");
+				tfName.setText("");
+				tfType.setText("");
+				tfPrice.setText("");
+				tfAmount.setText("");
+			}
+		});
+		btnClear.setBounds(215, 376, 89, 23);
+		contentPane.add(btnClear);
+		tableImportOrder.setModel(new javax.swing.table.DefaultTableModel(
+				 new Object [][] {
+
+			            },
+				 new String [] {
+
+			            }
+			));
+			
+			scrollPane.setViewportView(tableImportOrder);
+			
+			JLabel lblNewLabel_5 = new JLabel("Import Order ID:");
+			lblNewLabel_5.setBounds(10, 86, 99, 14);
+			contentPane.add(lblNewLabel_5);
+			
+			tfImportOrderID = new JTextField();
+			tfImportOrderID.setBounds(120, 83, 293, 20);
+			contentPane.add(tfImportOrderID);
+			tfImportOrderID.setColumns(10);
+		this.setLocationRelativeTo(null);
+		list = new ConnectImportOrder().getListimportOrder();
+		model = (DefaultTableModel) tableImportOrder.getModel();
+		model.setColumnIdentifiers(new Object[] {
+				"STT", "ID","Product ID", "Name", "Type", "Price", "Amount", "Date"
+		});
+		showTable();
+	}
+	int j = 1;
+	private JTextField tfImportOrderID;
+	public void showResult() {
+		importOrder i = list.get(list.size() -1 );
+		model.addRow(new Object[] {
+				j++,i.getIdI(),i.getId(), i.getName(), i.getType(), i.getPrice(), i.getAmount()
+		});
 	}
 }
