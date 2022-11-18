f8fa2e927c50f6caa531f1cb50e7551b31bb0e9e
dimmi213 <94824362+dimmi213@users.noreply.github.com>
2022-11-18 23:19:33 +0700

    Update ConnectExportOrder.java

--- ConnectExportOrder.java
+++ ConnectExportOrder.java
@@ -25,15 +25,16 @@ public class ConnectExportOrder {
 		System.out.print(conn);
 	}
 	public boolean addProducts(exportOrder i) {
-		String sql = "INSERT INTO tblExportOrder(ProductID, Name, Type, Price, Amount)"
-				+ "VALUES(?,?,?,?,?)";
+		String sql = "INSERT INTO tblExportOrder(ExportOrderID,ProductID, Name, Type, Price, Amount)"
+				+ "VALUES(?,?,?,?,?,?)";
 		try {
 			PreparedStatement ps = conn.prepareStatement(sql);
-			ps.setString(1, i.getId());
-			ps.setString(2, i.getName());
-			ps.setString(3, i.getType());
-			ps.setLong(4, i.getPrice());
-			ps.setLong(5, i.getAmount());
+			ps.setString(1, i.getIdE());
+			ps.setString(2, i.getId());
+			ps.setString(3, i.getName());
+			ps.setString(4, i.getType());
+			ps.setLong(5, i.getPrice());
+			ps.setLong(6, i.getAmount());
 //			ps.setLong(6, getDate);
 			return  ps.executeUpdate() > 0;
 		} catch (Exception e) {
@@ -53,6 +54,7 @@ public class ConnectExportOrder {
             ResultSet rs = ps.executeQuery();
             while(rs.next()) {
             	exportOrder i = new exportOrder();
+            	i.setIdE(rs.getString("ExportOrderID"));
             	i.setId(rs.getString("ProductID"));
             	i.setName(rs.getString("Name"));
             	i.setType(rs.getString("Type"));
