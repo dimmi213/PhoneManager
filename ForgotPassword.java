ac7b757df9236f2bca0c8158d3e5bc18d40a69a0
LinhDancute <117385818+LinhDancute@users.noreply.github.com>
2022-11-22 14:04:43 +0700

    Update ForgotPassword.java

--- ForgotPassword.java
+++ ForgotPassword.java
@@ -19,7 +19,6 @@ import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.awt.event.ActionEvent;
 
@@ -47,7 +46,9 @@ public class ForgotPassword extends JFrame {
 	}
 
 	Connection con;
-	PreparedStatement pst;
+	Statement pst;
+	private JTextField textEmail;
+	private JTextField textAnswer;
 
 	
 	/**
@@ -56,7 +57,7 @@ public class ForgotPassword extends JFrame {
 	public ForgotPassword() {
 		setTitle("Forgot Password");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 521, 412);
+		setBounds(100, 100, 617, 553);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
@@ -65,27 +66,27 @@ public class ForgotPassword extends JFrame {
 		
 		JLabel lblNewLabel = new JLabel("New Password System");
 		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 18));
-		lblNewLabel.setBounds(185, 38, 223, 37);
+		lblNewLabel.setBounds(185, 26, 223, 49);
 		contentPane.add(lblNewLabel);
 		
 		JLabel lblNewLabel_4 = new JLabel("New password:");
 		lblNewLabel_4.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblNewLabel_4.setBounds(74, 168, 116, 20);
+		lblNewLabel_4.setBounds(71, 289, 116, 20);
 		contentPane.add(lblNewLabel_4);
 		
 		textNewPassword = new JPasswordField();
 		textNewPassword.setFont(new Font("Tahoma", Font.PLAIN, 13));
-		textNewPassword.setBounds(216, 169, 237, 20);
+		textNewPassword.setBounds(213, 290, 195, 20);
 		contentPane.add(textNewPassword);
 		
 		JLabel lblNewLabel_5 = new JLabel("Confirm password:");
 		lblNewLabel_5.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblNewLabel_5.setBounds(74, 233, 131, 19);
+		lblNewLabel_5.setBounds(71, 354, 131, 19);
 		contentPane.add(lblNewLabel_5);
 		
 		textConfirm = new JPasswordField();
 		textConfirm.setFont(new Font("Tahoma", Font.PLAIN, 13));
-		textConfirm.setBounds(216, 232, 237, 20);
+		textConfirm.setBounds(213, 353, 195, 20);
 		contentPane.add(textConfirm);
 		
 		JButton btnNewButton = new JButton("Save");
@@ -94,6 +95,8 @@ public class ForgotPassword extends JFrame {
 				String username = textUsername.getText();
 				String newpassword = new String(textNewPassword.getPassword());
 				String confirm = new String(textConfirm.getPassword());
+				String email = textEmail.getText();
+				String answer = textAnswer.getText();
 				
 				StringBuilder cd = new StringBuilder();
 				
@@ -103,6 +106,13 @@ public class ForgotPassword extends JFrame {
 				
 				if(confirm.equals("")) {
 					cd.append("You haven't confirmed your new password!\n");
+				}
+				if(email.equals("")) {
+					cd.append("Please type your email!\n");
+				}
+				
+				if(answer.equals("")) {
+					cd.append("You haven't answered!\n");
 				}
 				
 				JFrame frame = new JFrame("JOptionPane showMessageDialog example");
@@ -114,55 +124,19 @@ public class ForgotPassword extends JFrame {
 				try {
 					Class.forName("com.mysql.cj.jdbc.Driver");
 					con = DriverManager.getConnection("jdbc:mySQL://localhost:3306/usermanagement","root","yunbrayyunh");
-					
-					Statement statement = con.createStatement();
-					Statement statement1 = con.createStatement();
-					Statement statement2 = con.createStatement();
-					
-					String query_IU = "SELECT * FROM usermanagement.import_user";
-					String query_EU = "SELECT * FROM usermanagement.export_user";
-					String query_A = "SELECT * FROM usermanagement.admin";
-					
-					ResultSet resultSet_iu = statement.executeQuery(query_IU);
-					ResultSet resultSet_eu = statement1.executeQuery(query_EU);
-					ResultSet resultSet_a = statement2.executeQuery(query_A);
-					
-					con.setHoldability(ResultSet.CLOSE_CURSORS_AT_COMMIT);
-
-					
-					if(resultSet_iu.next()) {
-						statement.executeUpdate("update import_user set Password ='"+newpassword+"' where Username ='"+username+"'");
-						if (newpassword.equals(confirm)) {
-							JOptionPane.showMessageDialog(null, "Your password is successfully updated");
-							setVisible(false);
-							new LoginForm().setVisible(true);
-						} else {
-							JOptionPane.showMessageDialog(frame, "The entered passwords do not match. Try again.", "Failture", JOptionPane.ERROR_MESSAGE);
-						}
-						resultSet_iu.close();
-					}
-					if(resultSet_eu.next()) {
-						statement1.executeUpdate("update export_user set Password ='"+newpassword+"' where Username ='"+username+"'");
-						if (newpassword.equals(confirm)) {
-							JOptionPane.showMessageDialog(null, "Your password is successfully updated");
-							setVisible(false);
-							new LoginForm().setVisible(true);
-							
-						} else {
-							JOptionPane.showMessageDialog(frame, "The entered passwords do not match. Try again.", "Failture", JOptionPane.ERROR_MESSAGE);
-						}
+					Statement pst = con.createStatement();
+					ResultSet rs = pst.executeQuery("select * from user where Username ='"+ username + "'and Email = '"+ answer+"'");
 						
-					}if(resultSet_a.next()) {
-					
-						statement2.executeUpdate("update admin set Password ='"+newpassword+"' where Username ='"+username+"'");
+					if(rs.next()) {
+						
 						if (newpassword.equals(confirm)) {
+							pst.executeUpdate("update user set Password = '"+ newpassword+ "' where Username = '"+ username + "'and Email ='"+ answer+"'");
 							JOptionPane.showMessageDialog(null, "Your password is successfully updated");
 							setVisible(false);
 							new LoginForm().setVisible(true);
 						} else {
 							JOptionPane.showMessageDialog(frame, "The entered passwords do not match. Try again.", "Failture", JOptionPane.ERROR_MESSAGE);
 						}
-						resultSet_a.close();
 					}
 					else {
 						JOptionPane.showMessageDialog(frame, "Please write correct Username or Answer", "Failture", JOptionPane.ERROR_MESSAGE);
@@ -179,7 +153,7 @@ public class ForgotPassword extends JFrame {
 			
 		});
 		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		btnNewButton.setBounds(96, 308, 89, 23);
+		btnNewButton.setBounds(93, 443, 89, 23);
 		contentPane.add(btnNewButton);
 		
 		JButton btnNewButton_1 = new JButton("Back");
@@ -190,18 +164,20 @@ public class ForgotPassword extends JFrame {
 			}
 		});
 		btnNewButton_1.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		btnNewButton_1.setBounds(399, 308, 89, 23);
+		btnNewButton_1.setBounds(396, 443, 89, 23);
 		contentPane.add(btnNewButton_1);
 		
 		JButton btnNewButton_2 = new JButton("Clear");
 		btnNewButton_2.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				textUsername.setText("");
+				textAnswer.setText("");
 				textNewPassword.setText("");
 				textConfirm.setText("");
 			}
 		});
 		btnNewButton_2.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		btnNewButton_2.setBounds(249, 310, 89, 23);
+		btnNewButton_2.setBounds(246, 445, 89, 23);
 		contentPane.add(btnNewButton_2);
 		
 		JLabel lblNewLabel_2_1 = new JLabel("Username:");
@@ -212,8 +188,31 @@ public class ForgotPassword extends JFrame {
 		textUsername = new JTextField();
 		textUsername.setFont(new Font("Tahoma", Font.PLAIN, 13));
 		textUsername.setColumns(10);
-		textUsername.setBounds(216, 97, 237, 20);
+		textUsername.setBounds(216, 97, 195, 20);
 		contentPane.add(textUsername);
+		
+		JLabel lblNewLabel_2_1_1 = new JLabel("Question Querity?");
+		lblNewLabel_2_1_1.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblNewLabel_2_1_1.setBounds(74, 159, 116, 20);
+		contentPane.add(lblNewLabel_2_1_1);
+		
+		textEmail = new JTextField();
+		textEmail.setText("What's your email?");
+		textEmail.setFont(new Font("Tahoma", Font.BOLD, 13));
+		textEmail.setColumns(10);
+		textEmail.setBounds(216, 158, 195, 20);
+		contentPane.add(textEmail);
+		
+		JLabel lblNewLabel_2_1_2 = new JLabel("Answer:");
+		lblNewLabel_2_1_2.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblNewLabel_2_1_2.setBounds(74, 225, 116, 14);
+		contentPane.add(lblNewLabel_2_1_2);
+		
+		textAnswer = new JTextField();
+		textAnswer.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		textAnswer.setColumns(10);
+		textAnswer.setBounds(216, 224, 195, 20);
+		contentPane.add(textAnswer);
 		
 		
 	}
